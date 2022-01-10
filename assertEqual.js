4f9dffbcb524b91971b0a9d2bef08a175bc182ac
a-sakhizada <arezousakhizada@gmail.com>
2022-01-10 00:26:10 -0500

    created our own console.assert function

---
+++ assertEqual.js
@@ -0,0 +1,32 @@
+//console.assert 
+// FUNCTION IMPLEMENTATION
+const sum = function(a, b) { 
+    return a + b;
+  }
+  
+  // TEST CODE
+  console.assert(sum(1, 2) === 3);
+  console.assert(sum(1, 20) === 3); // bad / incorrect assertion, and we see it fail!
+
+
+
+//our own assert function - can manipulate the color and symbol
+//Function implementation
+const assertEqual = function(actual, expected) {
+    if(actual === expected)
+    {
+        console.log("✅Assertion Passed: " + actual + " === " + expected); //to add emojis: winKey + ;
+    }
+    else
+    {
+        console.log("🚩Assertion Failed: " + actual + " !== " + expected);
+
+    }
+};
+
+//test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("happy", 'happy');
+assertEqual("HAPPY", "happy");
+assertEqual(12, 1 );
