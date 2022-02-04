08104a228100d7a75fa57ca7e2ad8382531088dc
a-sakhizada <arezousakhizada@gmail.com>
2022-02-04 00:31:28 -0500

    exporting our own lotide functions

---
+++ index.js
@@ -0,0 +1,13 @@
+//export 1 obt with all our functions 
+
+const head = require("./head");
+const tail   = require('./tail');
+const middle = require('./middle');
+
+module.exports = {
+    head: head,
+    tail: tail,
+    middle: middle
+};
+
+//console.log(module);
