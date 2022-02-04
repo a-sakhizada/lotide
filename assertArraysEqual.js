8d99b72b606262a1fdb99f8919337f9a0bb8d185
a-sakhizada <arezousakhizada@gmail.com>
2022-02-03 20:11:30 -0500

    modularized the codes and created their own test files

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -14,9 +14,4 @@ const assertArraysEqual = function(arr1, arr2) {
     return true;
 };
 
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
